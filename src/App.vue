ac13646f7a61b4d581af4126873f7f2953e5aae1
wangchanghua1995 <wangchanghua1113@163.com>
2017-12-10 15:02:42 +0800

    'first'

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <dog_header/>
+</template>
+
+<script>
+import header from './components/header/header.vue'
+export default {
+  components:{
+    'dog_header':header
+  },
+
+}
+</script>
+
+<style>
+
+</style>
